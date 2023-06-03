e24f8cc5bf7f65bffc83142077b23335678c83a2
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-06-04 00:08:54 +0100

    vm comment

--- src/vm/main.c
+++ src/vm/main.c
@@ -1,3 +1,7 @@
+// for now, the VM is just going to interpret the bytecode
+// we might add a JIT compiler later, but that's a little overengineered for now
+// jvm hotspot interprets and then switches to JIT if a method is called a lot
+
 int main(void) {
     return 0;
 }
